953c7202f6c5b35a83728eee783a9ff3b40511af
joschonarth <joschonarth@gmail.com>
2024-07-19 19:09:46 -0300

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -78,7 +78,7 @@ $( document ).ready(function() {
       step: function(state, circle) {
         circle.path.setAttribute('stroke', state.color);
   
-        var value = Math.round(circle.value() * 546741);
+        var value = Math.round(circle.value() * 5423);
         circle.setText(value);
   
       }
@@ -146,5 +146,42 @@ $( document ).ready(function() {
             });
         }
     }
+
+    // Scroll para as seções
+
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+
+    let btnId = $(this).attr('id');
+
+    if(btnId == 'about-menu') {
+      scrollTo = aboutSection;
+    } else if(btnId == 'services-menu') {
+      scrollTo = servicesSection;
+    } else if(btnId == 'team-menu') {
+      scrollTo = teamSection;
+    } else if(btnId == 'portfolio-menu') {
+      scrollTo = portfolioSection;
+    } else if(btnId == 'contact-menu') {
+      scrollTo = contactSection;
+    } else {
+      scrollTo = bannerSection;
+    }
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(scrollTo).offset().top - 70
+    }, 1500);
+
+    });
  
 });
